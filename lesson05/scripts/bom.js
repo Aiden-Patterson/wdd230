cea319f091b17fc9628f829c7ced5fed7eb0b0b0
aiden-patterson <pat18001@byui.edu>
2024-02-08 13:43:42 -0700

    created bom, added to main page

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,20 +1,20 @@
-const input = document.querySelector('#favchapter');
-const button = document.querySelector('#button');
-const list = document.querySelector('#list');
+const input = document.querySelector("#favchap");
+const button = document.querySelector("#submit");
+const list = document.querySelector("#list");
 
-button.addEventListener('click', ()=> {
-    if(input.value != ''){
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        li.append(deleteButton);
-        deleteButton.addEventListener('click', ()=> {
-            li.remove();
-            input.focus();
-        });
-        list.append(li);
-        input.focus();
-        input.value = '';
-    }
+button.addEventListener("click", () => {
+  if (input.value != "") {
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    li.textContent = input.value;
+    deleteButton.textContent = "❌";
+    li.append(deleteButton);
+    deleteButton.addEventListener("click", () => {
+      li.remove();
+      input.focus();
+    });
+    list.append(li);
+    input.focus();
+    input.value = "";
+  }
 });
