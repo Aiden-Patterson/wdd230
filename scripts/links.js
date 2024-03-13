31eae67ed1ef04ba36e51249e9aee3163e427965
aiden-patterson <pat18001@byui.edu>
2024-03-12 18:49:27 -0600

    completed links.js

--- scripts/links.js
+++ scripts/links.js
@@ -1,25 +1,30 @@
 const baseURL = "https://Aiden-Patterson.github.io/wdd230/"
 const linksURL = "https://aiden-patterson.github.io/wdd230/data/links.json";
-const unorderedList = document.querySelector('assignments');
+const unorderedList = document.getElementById('assignments');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    console.log(data.lessons);
+    displayLinks(data.lessons);
 }
 
 const displayLinks = (weeks) => {
     weeks.forEach((lesson) => {
+        console.log(lesson);
         const weekNumber = lesson.lesson;
-        lesson.forEach((assignment) => {
+        lesson.links.forEach((assignment) => {
+        
             const url = assignment.url;
             const title = assignment.title;
             const link = document.createElement('a');
             link.textContent = weekNumber + " : " + title;
             link.setAttribute('href', url);
-            const list = createElement('li');
+            const list = document.createElement('li');
             list.appendChild(link);
+            console.log(assignment);
             unorderedList.appendChild(list);
+            
         });
     });
 }
