6e0cd607af297560e79f680e9510a7820afcda4e
aiden-patterson <pat18001@byui.edu>
2024-04-08 17:57:42 -0600

    fixed fruits.json

--- Final/scripts/fruits.js
+++ Final/scripts/fruits.js
@@ -1,8 +1,17 @@
 const option1 = document.getElementById('fruit1');
 const option2 = document.getElementById('fruit2');
 const option3 = document.getElementById('fruit3');
-const url = "data/fruits.js";
+const url = "https://github.com/Aiden-Patterson/wdd230/tree/main/Final/data/fruits.json";
 
+
+async function getFruits(){
+    console.log("hello");
+    const response = await fetch(url);
+    console.log("world");
+    const data = await response.json();
+    //console.log(data.fruits);
+    displayFruits(data.fruits);
+}
 function displayFruits(){
     console.log(url);
     url.forEach((fruit) => {
@@ -15,4 +24,4 @@ function displayFruits(){
     });
 }
 
-displayFruits();
+getFruits();
